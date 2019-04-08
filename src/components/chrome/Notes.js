d173cd0c8a98f99c20dc700ecd0b0e02566c3b3c
Willem Van Bockstal <willem@spatie.be>
2019-04-08 15:26:39 +0200

    Notes without pages

--- src/components/chrome/Notes.js
+++ src/components/chrome/Notes.js
@@ -1,14 +1,16 @@
 import useWires from "../../hooks/useWires";
 
 export default function Note() {
-  const { notes, showNotes, toggleShowNotes } = useWires();
+  const { notes, pages, showNotes, toggleShowNotes } = useWires();
 
   return (
     <>
       {notes.length > 0 && !showNotes && (
         <button
           onClick={toggleShowNotes}
-          className="absolute z-10 top-0 left-0 w-6 h-6 -mt-3 -ml-3 grid place-items-center rounded-full font-mono leading-none text-xs text-white bg-red-500 text-xs"
+          className={`${
+            pages.length ? "absolute -mt-3 -ml-3" : ""
+          } z-10 top-0 left-0 w-6 h-6 grid place-items-center rounded-full font-mono leading-none text-xs text-white bg-red-500 text-xs`}
         >
           {notes.length}
         </button>
