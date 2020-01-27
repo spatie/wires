2195941d027eb04cf96b98febdd595a82b880e51
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2020-01-27 17:22:27 +0100

    Update withTM setup

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 const withTM = require('next-transpile-modules');
 
 module.exports = (nextConfig = {}) => {
-    return withTM({
-        transpileModules: ['@spatie/wires'],
-
-        ...nextConfig,
-    });
+    return withTM(['@spatie/wires'])(nextConfig);
 };
